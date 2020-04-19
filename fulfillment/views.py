5df1f5e296c0b562840ca42ef8e2c88eaccea597
minsookim <minsookim0615@gmail.com>
2020-04-19 19:10:03 +0900

    Heroku Setting

--- fulfillment/views.py
+++ fulfillment/views.py
@@ -20,16 +20,21 @@ from firebase_admin import firestore
 import pathlib
 from firebase_admin import storage
 path = str(pathlib.Path().absolute())
-privateKey = os.environ.get("FIREBASE_PRIVATE_KEY")
-clientEmail = os.environ.get("FIREBASE_CLIENT_EMAIL")
 # cred = credentials.Certificate(os.environ.get("FIREBASE_TOKEN"))
 default_app = firebase_admin.initialize_app({
     credentials: firebase_admin.credentials.Certificate({
-        "type" : "service_account",
-        "private_key" : privateKey,
-        "client_email": clientEmail
+      "type": "service_account",
+      "project_id": "reindeer-fulfillment",
+      "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+      "private_key": os.environ.get("FIREBASE_PRIVATE_KEY"),
+      "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+      "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URI")
     })
-});
+})
 
 paramEvents = Events()
 paramActions = Actions()
